39822ca9b4970e9ed749e16c8eeba51506e4bf21
punkrockviking <danjeccles@gmail.com>
2021-04-15 13:58:06 -0600

    (feat) use dangerouslySetInnerHTML only when you trust source

--- src/components/Search.js
+++ src/components/Search.js
@@ -26,6 +26,19 @@ const Search = () => {
       search();
     }, [term]);
 
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }} ></span>
+                </div>
+            </div>
+        );
+    });
+
     return (
         <div>
             <div className="ui form">
@@ -38,6 +51,9 @@ const Search = () => {
                     />
                 </div>
             </div>
+                <div className="ui celled list">
+                    {renderedResults}
+                </div>
         </div>
     );
 };
