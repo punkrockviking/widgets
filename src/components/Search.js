c09ad6ba28dd2dc490282e83cf71af980d2335fc
punkrockviking <danjeccles@gmail.com>
2021-04-15 13:36:09 -0600

    (feat) implemented axios into function based component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
 
-    console.log('I RUN WITH EVERY RENDER')
+    console.log(results)
 
     useEffect(() => {
-        console.log('I RUN AFTER some shit');
+      const search = async () => {
+        const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+              params: {
+                  action: 'query',
+                  list: 'search',
+                  origin: '*',
+                  format: 'json',
+                  srsearch: term,
+              }
+          });
+
+          setResults(data.query.search);
+      };
+
+      search();
     }, [term]);
 
     return (
         <div>
-            <div classname="ui form">
+            <div className="ui form">
                 <div className="field">
                     <label>Enter Search Term</label>
                     <input 
